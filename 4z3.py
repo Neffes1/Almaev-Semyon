2152779d87b5b3d84591fcbb1589ecb2d7afbf1a
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:03:44 +0300

    Delete 4z3.py

--- 4z3.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Введите число A:")) 
-b=int(input("Введите число B:")) 
-
-if a > b:
-  for i in range(a, b-1, -1):
-    if i % 2 != 0:
-      print(i)
-else:
-  print("a должно быть больше b")
