aeedb671925ce9b5371726ea95e55dab2d92f3d2
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:00:01 +0300

    Update 4z3.py

--- Занятие 4/4z3.py
+++ Занятие 4/4z3.py
@@ -1,1 +1,9 @@
+a=int(input("Введите число A:")) 
+b=int(input("Введите число B:")) 
 
+if a > b:
+  for i in range(a, b-1, -1):
+    if i % 2 != 0:
+      print(i)
+else:
+  print("a должно быть больше b")
