d0181078286109f23c9b24af7f14a527386105de
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:17:07 +0300

    Update 5z8.py

--- Занятие 5/5z8.py
+++ Занятие 5/5z8.py
@@ -1,1 +1,14 @@
+def F(n):
+    a = 0
+    previous = None
+    while True:
+        if n == 0:
+            break  
+        if previous is not None and n == previous:
+            a += 1
+        previous = n
+    print(a)
 
+n = int(input())
+
+F(n)
