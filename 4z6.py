4e8e33e4e6e979cbf491fdb292490e707b911dc3
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:03:59 +0300

    Delete 4z6.py

--- 4z6.py
+++
@@ -1,9 +0,0 @@
-n=int(input("n="))
-if n < 0:
-    print("Факториал только для натуральных чисел")
-else:
-    factorial = 1
-    for i in range(1, n+1):
-        factorial *= i
-
-    print("n= ", factorial)
