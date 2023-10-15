f3faca7f18464554f80decb1c188105347439b51
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:01:12 +0300

    Update 4z7.py

--- Занятие 4/4z7.py
+++ Занятие 4/4z7.py
@@ -1,1 +1,12 @@
+n=int(input("Введите n:"))
+if n < 0:
+    print("n должно быть натуральным числом")
+else:
+    factorial = 1
+    sum = 0
 
+    for i in range(1, n+1):
+        factorial *= i
+        sum += factorial
+
+    print(sum)
