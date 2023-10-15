e0f43e28ceb9bf3ca16aad7ac7a91f9ecbf703f2
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-15 14:01:25 +0300

    Update 4z8.py

--- Занятие 4/4z8.py
+++ Занятие 4/4z8.py
@@ -1,1 +1,8 @@
-
+n=int(input("Введите натуральное число меньше девяти"))
+if 1 <= n <= 9:
+    for i in range(1, n+1):
+        for k in range(1, i+1):
+            print(k, end="")
+        print()
+else:
+    print("ошибка")
