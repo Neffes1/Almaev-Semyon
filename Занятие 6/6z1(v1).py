c735eb038621c08801eb3335e048679bd92891d7
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-29 23:17:05 +0300

    Update 6z1(v1).py

--- Занятие 6/6z1(v1).py
+++ Занятие 6/6z1(v1).py
@@ -9,3 +9,6 @@ def count_words_starting_with_e(text):
 text = "Дана строка с русскоязычным текстом. Найти слова, начинающиеся с буквы е (или ё)."
 result = count_words_starting_with_e(text)
 print("Количество слов, начинающихся с 'е' или 'ё':", result)
+
+# Вызов функции
+main()
