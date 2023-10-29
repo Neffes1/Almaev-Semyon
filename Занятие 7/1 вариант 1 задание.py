493b910ec71bec4873f6d29c75b37ec840044ce1
Semyon Almaev <144039557+Neffes1@users.noreply.github.com>
2023-10-29 23:15:10 +0300

    Update 1 вариант 1 задание.py

--- Занятие 7/1 вариант 1 задание.py
+++ Занятие 7/1 вариант 1 задание.py
@@ -1,7 +1,15 @@
-arr=[]
-n = 5
-for i in range(n):
-    arr.append(input('Введите число: '))
- 
-print('максимальный элемент: ' + str(max(arr)))
-print('обратный порядок: ' + str(list(reversed(arr))))
+def find_max_and_reverse_array():
+    n = int(input("Введите размер массива: "))
+    arr = []
+    for i in range(n):
+        element = int(input(f"Введите элемент {i + 1}: "))
+        arr.append(element)
+    
+    max_element = max(arr)
+    print(f"Максимальный элемент: {max_element}")
+    
+    reversed_arr = arr[::-1]
+    print("Массив в обратном порядке:", reversed_arr)
+
+# Вызов функции
+find_max_and_reverse_array()
